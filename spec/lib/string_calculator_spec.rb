28760cae7de17be5e140b99dc49c80457de5d71e
Abhay Pratap Singh <abhaykeepworking@gmail.com>
2025-11-13 13:46:06 +0530

    Step 2: handle single number input

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -6,5 +6,8 @@ RSpec.describe StringCalculator do
     it 'returns 0 for empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+    it 'returns the number itself when one number is given' do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
   end
 end
